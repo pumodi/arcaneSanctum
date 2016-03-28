821c355631723ade9d478cab9815243662f3c1df
Jeffrey Brice <jeffbricemusic@gmail.com>
2016-03-28 02:52:43 -0700

    Added index.html and min.'s

---
+++ main.js
@@ -0,0 +1,94 @@
+//Variables
+var buttonsClicked = 0;
+var cursors = 0;
+var mindControlSpells = 0;
+
+//Save game logic
+var save = {
+  buttonsClicked: buttonsClicked,
+  cursors: cursors,
+}
+
+localStorage.setItem("save",JSON.stringify(save));
+
+var savegame = JSON.parse(localStorage.getItem("save"));
+if (typeof savegame.buttonsClicked !== "undefined") buttonsClicked = savegame.buttonsClicked;
+if (typeof savegame.cursors !== "undefined") cursors = savegame.cursors;
+
+//Prettify stuff
+function cleanRogueDecimals(input){
+  var output = Math.round(input * 1000000)/1000000;
+    return output;
+};
+
+//Clickables
+
+function autoClick(number){
+  buttonsClicked = buttonsClicked + number;
+  document.getElementById("clickTotal").innerHTML = cleanRogueDecimals(buttonsClicked);
+  gameState();
+}
+
+function buttonClick(number){
+  buttonsClicked = buttonsClicked + number;
+  document.getElementById("clickTotal").innerHTML = cleanRogueDecimals(buttonsClicked);
+  document.getElementById("alertBox").innerHTML = "";
+  gameState();
+};
+
+function buyCursor(){
+  document.getElementById("alertBox").innerHTML = "";
+  var cursorCost = Math.floor(10 * Math.pow(1.1,cursors));
+  if(buttonsClicked >= cursorCost){
+    cursors = cursors + 1;
+    buttonsClicked = buttonsClicked - cursorCost;
+    document.getElementById("cursors").innerHTML = cleanRogueDecimals(cursors);
+    document.getElementById("clickTotal").innerHTML = cleanRogueDecimals(buttonsClicked);
+  }
+  else {
+    document.getElementById("alertBox").innerHTML = "You don't have enough arcane knowledge to cast an Auto-Read spell...";
+  };
+  var nextCost = Math.floor(10 * Math.pow(1.1,cursors));
+  document.getElementById("cursorCost").innerHTML = cleanRogueDecimals(nextCost);
+  gameState();
+};
+
+function buyMindControl(){
+  document.getElementById("alertBox").innerHTML = "";
+  var mindControlCost = Math.floor(100 * Math.pow(1.1,mindControlSpells));
+  if(buttonsClicked >= mindControlCost){
+    mindControlSpells = mindControlSpells + 1;
+    buttonsClicked = buttonsClicked - mindControlCost;
+    document.getElementById("mindControlSpells").innerHTML = cleanRogueDecimals(mindControlSpells);
+    document.getElementById("clickTotal").innerHTML = cleanRogueDecimals(buttonsClicked);
+  }
+  else {
+    document.getElementById("alertBox").innerHTML = "You don't have enough arcane knowledge to cast a Mind Control spell...";
+  };
+  var nextCost = Math.floor(100 * Math.pow(1.1,mindControlSpells));
+  document.getElementById("mindControlCost").innerHTML = cleanRogueDecimals(nextCost);
+  gameState();
+};
+
+// Game progression switch
+function gameState() {
+  if (buttonsClicked > 0) {
+    document.getElementById("dialogueBox").innerHTML = "The book appears to be a spellbook of some kind. You feel compelled to read further...";
+    document.getElementById("autoReadSpellDiv").style.visibility = "visible";
+  };
+  if (cursors > 0) {
+    document.getElementById("dialogueBox").innerHTML = "A power overwhelming poured out of you as you read the arcane lettering. The pages are turning themselves now yet somehow you still seem to know what they contain...";
+  };
+  if (cursors > 5) {
+    document.getElementById("dialogueBox").innerHTML = "After pouring through the volume, you find that it is possible to control someone elses mind. Perhaps you could use this to help you gain even more knowledge. If only you knew how to cast such a spell. Must keep reading...";
+    document.getElementById("mindControlSpellDiv").style.visibility = "visible";
+  };
+  document.getElementById("pagePerSec").innerHTML = (cursors + (mindControlSpells *10));
+};
+
+// Handles time based effects
+window.setInterval(function() {
+  autoClick(cursors);
+  autoClick(mindControlSpells * 10);
+  gameState();
+}, 1000);
